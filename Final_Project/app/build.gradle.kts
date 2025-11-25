02eac00b0347d47f8ef9abfee802dd8312faba80
Minho Lee <3570js@naver.com>
2025-11-26 00:07:03 +0900

    Refactor 코드 삭제

--- Final_Project/app/build.gradle.kts
+++ Final_Project/app/build.gradle.kts
@@ -54,7 +54,6 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
-    implementation("net.nurigo:sdk:4.3.0")
 
 
 }
